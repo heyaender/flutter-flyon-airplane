9a19ba5b18c2f071d700c6f44fb936d7e164ca67
Andrianto Cahyono <andri.c.putro@gmail.com>
2022-11-27 01:58:15 +0700

    adding themes

---
+++ lib/shared/theme.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xFFFFFFFF);
+Color whiteCloudColor = Color(0xFFFAFAFA);
+Color purpleColor = Color(0xFF5C40CC);
+Color purpleDarkColor = Color(0xFF1F1449);
+Color greyColor = Color(0xFF9698A9);
+Color greyLightColor = Color(0xFFDBD7EC);
+Color greenColor = Color(0xFF0EC3AE);
+Color pinkColor = Color(0xFFEB70A5);
+
+TextStyle darkText = GoogleFonts.poppins(
+  color: purpleDarkColor,
+);
+
+TextStyle whiteText = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
+TextStyle greyText = GoogleFonts.poppins(
+  color: greyColor,
+);
+
+TextStyle greenText = GoogleFonts.poppins(
+  color: greenColor,
+);
+
+TextStyle pinkText = GoogleFonts.poppins(
+  color: pinkColor,
+);
+
+TextStyle purpleText = GoogleFonts.poppins(
+  color: purpleColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
