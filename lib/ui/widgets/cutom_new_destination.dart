f7901f043fa7a6af4af61bdde240345e933fcf4c
Andrianto Cahyono <andri.c.putro@gmail.com>
2022-12-13 15:17:49 +0700

    slicing custom new destination

--- lib/ui/widgets/cutom_new_destination.dart
+++ lib/ui/widgets/cutom_new_destination.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flyon_airplane_app/shared/theme.dart';
+import 'package:flyon_airplane_app/ui/pages/detail_page.dart';
 
 class CustomNewDestination extends StatelessWidget {
   const CustomNewDestination({
@@ -19,86 +20,98 @@ class CustomNewDestination extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 16),
-      width: 327,
-      height: 90,
-      decoration: BoxDecoration(
-        color: whiteColor,
-        borderRadius: BorderRadius.circular(18),
-      ),
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            width: 70,
-            height: 70,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              image: DecorationImage(
-                  image: AssetImage(
-                    imageUrl,
-                  ),
-                  fit: BoxFit.cover),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
           ),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(
-                left: 6,
-                top: 18,
-                bottom: 19,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: darkText.copyWith(
-                      fontSize: 18,
-                      fontWeight: medium,
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 16,
+        ),
+        width: 327,
+        height: 90,
+        decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                    image: AssetImage(
+                      imageUrl,
                     ),
-                    overflow: TextOverflow.ellipsis,
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(
+                  left: 6,
+                  top: 18,
+                  bottom: 19,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: darkText.copyWith(
+                        fontSize: 18,
+                        fontWeight: medium,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      city,
+                      style: greyText.copyWith(
+                        fontWeight: light,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                right: 16,
+              ),
+              width: 46,
+              height: 24,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 16,
+                    color: yellowColor,
                   ),
                   SizedBox(
-                    height: 5,
+                    width: 2,
                   ),
                   Text(
-                    city,
-                    style: greyText.copyWith(
-                      fontWeight: light,
+                    rating.toString(),
+                    style: darkText.copyWith(
+                      fontWeight: medium,
                     ),
-                  ),
+                  )
                 ],
               ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(
-              right: 16,
-            ),
-            width: 46,
-            height: 24,
-            child: Row(
-              children: [
-                Icon(
-                  Icons.star,
-                  size: 16,
-                  color: yellowColor,
-                ),
-                SizedBox(
-                  width: 2,
-                ),
-                Text(
-                  rating.toString(),
-                  style: darkText.copyWith(
-                    fontWeight: medium,
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
